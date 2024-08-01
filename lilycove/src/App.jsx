5f249bd642ef815b1485ad0867ac60f725c3720d
John Higgins <johnmatthiggins@gmail.com>
2024-07-31 17:20:45 -0700

    centered little dialog

--- lilycove/src/App.jsx
+++ lilycove/src/App.jsx
@@ -1,5 +1,5 @@
 import styles from './App.module.css';
-import { createEffect, createSignal } from 'solid-js';
+import { createEffect, createSignal, Show } from 'solid-js';
 
 const ASCII_UPPER_CASE_A = 0x41;
 const ASCII_LOWER_CASE_A = 0x61;
@@ -14,15 +14,12 @@ const PKMN_BANG = 0xab;
 function convertPokemonCharToASCII(pokemonChar) {
   if (pokemonChar >= PKMN_UPPER_CASE_A && pokemonChar < PKMN_LOWER_CASE_A) {
     const charCode = ASCII_UPPER_CASE_A + (Number(pokemonChar) - PKMN_UPPER_CASE_A);
-    console.log(String.fromCharCode(Number(charCode)));
     return String.fromCharCode(Number(charCode));
   } else if (pokemonChar >= PKMN_LOWER_CASE_A && Number(pokemonChar) < PKMN_LEFT_ARROW) {
     const charCode = ASCII_LOWER_CASE_A + (Number(pokemonChar) - PKMN_LOWER_CASE_A);
-    console.log(String.fromCharCode(Number(charCode)));
     return String.fromCharCode(Number(charCode));
   } else if (pokemonChar >= PKMN_ZERO && pokemonChar < PKMN_BANG) {
     const charCode = ASCII_ZERO + (Number(pokemonChar) - PKMN_ZERO);
-    console.log(String.fromCharCode(Number(charCode)));
     return String.fromCharCode(Number(charCode));
   }
   return ' ';
@@ -54,36 +51,58 @@ function parseTrainerId(bytes) {
   return bytesToNumber(bytes);
 }
 
+const SAVE_B_OFFSET = 0x00E000;
+const TRAINER_NAME_POSITION = 0x6000;
+
 function App() {
   const [bits, setBits] = createSignal([]);
   const [trainerName, setTrainerName] = createSignal('');
   const [trainerId, setTrainerId] = createSignal('');
+  const [trainerGender, setTrainerGender] = createSignal('?');
 
   createEffect(() => {
-    const newTrainerName = parseTrainerName(bits().slice(0x6000, 0x6008));
+    const offsetA = TRAINER_NAME_POSITION;
+    const offsetB = SAVE_B_OFFSET + TRAINER_NAME_POSITION;
+    const trainerNameA = parseTrainerName(bits().slice(offsetA, offsetA + 7));
+    const trainerNameB = parseTrainerName(bits().slice(offsetB, offsetB + 7));
     const newTrainerId = parseTrainerId(bits().slice(0x600a, 0x600c));
-    setTrainerName(newTrainerName);
+
+    let gender = 'M';
+    if (bits[TRAINER_NAME_POSITION + 7] === 1) {
+      gender = 'F';
+    }
+
+    setTrainerGender(gender)
+    setTrainerName(trainerNameA.trim() || trainerNameB);
     setTrainerId(newTrainerId);
   });
 
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <div class="rounded-lg bg-cyan-200 w-64 mx-auto">
-          <h3 class="text-3xl">Lilycove City</h3>
-          <h2 class="text-2xl">A Generation III Hex Editor</h2>
-          <input
-            type="file"
-            accept=".sav"
-            onChange={async (e) => {
-              const file = e.target.files[0];
-              const bytes = await file.bytes();
-              setBits(bytes);
-            }} />
-          <pre class="text-left whitespace-pre">Trainer Name: {trainerName()}</pre>
-          <pre class="text-left whitespace-pre">Trainer ID: {trainerId()}</pre>
+    <div class="h-full justify-center">
+      <div class="flex h-full justify-center items-center">
+        <div class="rounded-lg bg-cyan-200 p-2">
+          <h3 class="text-3xl font-bold">Lilycove City</h3>
+          <h2 class="text-md">A Generation III Hex Editor</h2>
+          <label>
+            <span class="text-white px-2 py-1 bg-teal-400 rounded-md hover:cursor-pointer" role="button">Upload Save</span>
+            <input
+              type="file"
+              accept=".sav"
+              class="hidden"
+              onChange={async (e) => {
+                const file = e.target.files[0];
+                const bytes = await file.bytes();
+                setBits(bytes);
+              }} />
+          </label>
+          <Show when={bits().length}>
+            <h3 class="text-2xl">Trainer Data</h3>
+            <pre class="text-left whitespace-pre">Name: {trainerName()}</pre>
+            <pre class="text-left whitespace-pre">ID: {trainerId()}</pre>
+            <pre class="text-left whitespace-pre">Gender: {trainerGender()}</pre>
+          </Show>
         </div>
-      </header>
+      </div>
     </div>
   );
 }
