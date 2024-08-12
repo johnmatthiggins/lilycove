2869120a9c6cf31085bb1bd9378e74c758999775
John Higgins <johnmatthiggins@gmail.com>
2024-08-11 20:27:13 -0700

    added names to columns in movelist script

--- scripts/movelist.py
+++ scripts/movelist.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from io import StringIO
 import httpx
 import pandas as pd
 
@@ -22,17 +23,22 @@ MOVE_TYPES = [
     'water',
 ]
 
-BASE_URL = 'https://www.serebii.net/attackdex/'
+BASE_URL = 'https://www.serebii.net/attackdex/%s.shtml'
 
 def main():
-    response = httpx.get(BASE_URL)
-    html = response.text
-    df = pd.read_html(io=html)[0].dropna()
-    df = df[df.iloc[:, 7].str.match('^(I|II|III)$')].set_axis(
-        ['move_id', 'name', 'type', 'category', 'pp', 'power', 'accuracy', 'gen'],
-        axis=1,
-    )
-    print(df)
+    for move_type in MOVE_TYPES:
+        url = BASE_URL % move_type
+        print('hitting %s' % url)
+        response = httpx.get(BASE_URL % move_type)
+        html = response.text
+        tables = pd.read_html(io=StringIO(html), match='Effect')
+        move_table = tables[1].drop([0], axis=0)
+        move_table.columns = ['name', 'type', 'PP', 'power', 'accuracy', 'effect', 'contest_type']
+        print(move_table)
+        # df = df[df.iloc[:, 7].str.match('^(I|II|III)$')].set_axis(
+        #     ['move_id', 'name', 'type', 'category', 'pp', 'power', 'accuracy', 'gen'],
+        #     axis=1,
+        # )
 
 if __name__ == '__main__':
     main()
