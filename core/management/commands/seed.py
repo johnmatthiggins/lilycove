441324023ec955b3f68e8c909b72e018ce8ef3d6
John Higgins <johnmatthiggins@gmail.com>
2024-08-12 11:59:39 -0700

    need to finish join script for move data and indexes

--- core/management/commands/seed.py
+++ core/management/commands/seed.py
@@ -20,7 +20,11 @@ MOVE_TYPES = [
     "ice",
     "normal",
     "poison",
-    "psychic",
+
+    # there is an attack named psychic so
+    # basically we have to append 't' to get the
+    # list of psychic type attacks.
+    "psychict",
     "rock",
     "steel",
     "water",
@@ -39,45 +43,44 @@ class Command(BaseCommand):
         html = response.text
 
         # find the table on the screen that has "master ball" inside it...
-        item_table = pd.read_html(io=StringIO(html), match="Master Ball")[0].drop(
-            index=[0, 1, 3], axis=1
+        item_table = (pd
+            .read_html(io=StringIO(html), match="Master Ball")[0]
+            .drop(
+                index=[0, 1, 3], axis=1
+            )
         )
-        item_table.columns = ["id", "hex_id", "bag_sprite", "item_description"]
-
-        item_table = item_table.drop(
-            index=item_table[
-                item_table["item_description"].str.contains("unknown", regex=False)
-            ].index
-        )
-        item_table = item_table.drop(
-            index=item_table[
-                item_table["item_description"].str.contains("Nothing", regex=False)
-            ].index
-        )
-        item_table = item_table.drop(
-            index=item_table[item_table["id"].str.contains("#", regex=False)].index
+        item_table.columns = ["id", "hex_id", "bag_sprite", "desc"]
+        item_table = (
+            item_table.drop(
+                index=item_table[item_table["desc"].str.contains("unknown", regex=False)].index
+            )
+            .drop(
+                index=item_table[item_table["desc"].str.contains("Nothing", regex=False)].index
+            )
+            .drop(
+                index=item_table[item_table["id"].str.contains("#", regex=False)].index
+            )
         )
 
         new_items = item_table.apply(
-            lambda r: (
-                Item(
-                    id=int(r["id"]),
-                    name=r["item_description"],
-                )
-            ),
+            lambda r: (Item(id=int(r["id"]), name=r["desc"])),
             axis=1,
         )
         Item.objects.bulk_create(list(new_items))
 
     def _load_moves(self):
         base_url = "https://www.serebii.net/attackdex/%s.shtml"
-        
         # fetch bulbapedia list of moves to get indexes...
         # combine name and convert it to lower case to compare
         # and avoid issues with names like dynamicpunch
+        new_moves = []
 
         for move_type in MOVE_TYPES:
             response = httpx.get(base_url % move_type)
+
+            # kinda weird but whatever...
+            if move_type == 'psychict':
+                move_type = 'psychic'
 
             html = response.text
             tables = pd.read_html(io=StringIO(html), match="Effect")
@@ -103,12 +106,30 @@ class Command(BaseCommand):
                 ),
                 axis=1,
             )
-            moves = list(moves)
 
-            # insert them into the database
-            Move.objects.bulk_create(moves)
+            new_moves += list(moves)
+
+        bulbapedia_url = 'https://bulbapedia.bulbagarden.net/wiki/List_of_moves'
+
+        response = httpx.get(bulbapedia_url)
+        html = response.text
+
+        # grab move table
+        move_table = pd.read_html(io=StringIO(html), match="Pound")[0].drop([0, 1], axis=1)
+        move_table.columns = [
+            'id', 'name', 'move_type', 'category', 'pp', 'power', 'accuracy', 'generation'
+        ]
+        move_table = move_table[
+            move_table["generation"].str.contains("^I$|^II$|^III$", regex=True)
+        ]
+        print(move_table)
+
+        # insert them into the database
+        # Move.objects.bulk_create(new_moves)
+
+
 
     def handle(self, *args, **options):
-        self._load_items()
-        # self._load_moves()
+        # self._load_items()
+        self._load_moves()
         self._load_species()
