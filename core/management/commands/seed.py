a0ceb1a84206f50ab78579b6b6d88823b3997a4e
John Higgins <johnmatthiggins@gmail.com>
2024-08-12 18:23:56 -0700

    added fuzzy matching to get ids of moves

--- core/management/commands/seed.py
+++ core/management/commands/seed.py
@@ -3,6 +3,7 @@ from core.models import Move, Item
 
 from io import StringIO
 
+from thefuzz import fuzz
 import httpx
 import pandas as pd
 
@@ -20,7 +21,6 @@ MOVE_TYPES = [
     "ice",
     "normal",
     "poison",
-
     # there is an attack named psychic so
     # basically we have to append 't' to get the
     # list of psychic type attacks.
@@ -43,19 +43,20 @@ class Command(BaseCommand):
         html = response.text
 
         # find the table on the screen that has "master ball" inside it...
-        item_table = (pd
-            .read_html(io=StringIO(html), match="Master Ball")[0]
-            .drop(
-                index=[0, 1, 3], axis=1
-            )
+        item_table = pd.read_html(io=StringIO(html), match="Master Ball")[0].drop(
+            index=[0, 1, 3], axis=1
         )
         item_table.columns = ["id", "hex_id", "bag_sprite", "desc"]
         item_table = (
             item_table.drop(
-                index=item_table[item_table["desc"].str.contains("unknown", regex=False)].index
+                index=item_table[
+                    item_table["desc"].str.contains("unknown", regex=False)
+                ].index
             )
             .drop(
-                index=item_table[item_table["desc"].str.contains("Nothing", regex=False)].index
+                index=item_table[
+                    item_table["desc"].str.contains("Nothing", regex=False)
+                ].index
             )
             .drop(
                 index=item_table[item_table["id"].str.contains("#", regex=False)].index
@@ -69,6 +70,25 @@ class Command(BaseCommand):
         Item.objects.bulk_create(list(new_items))
 
     def _load_moves(self):
+        bulbapedia_url = "https://bulbapedia.bulbagarden.net/wiki/List_of_moves"
+
+        response = httpx.get(bulbapedia_url)
+        html = response.text
+
+        # grab move table
+        move_table = pd.read_html(io=StringIO(html), match="Pound")[0].drop([0,1])
+        move_table.columns = [
+            "id",
+            "name",
+            "move_type",
+            "category",
+            "pp",
+            "power",
+            "accuracy",
+            "generation",
+        ]
+        move_pairings = list(move_table.apply(lambda r: [r["name"].lower().replace(' ', ''), int(r["id"])], axis=1))
+
         base_url = "https://www.serebii.net/attackdex/%s.shtml"
         # fetch bulbapedia list of moves to get indexes...
         # combine name and convert it to lower case to compare
@@ -79,8 +99,8 @@ class Command(BaseCommand):
             response = httpx.get(base_url % move_type)
 
             # kinda weird but whatever...
-            if move_type == 'psychict':
-                move_type = 'psychic'
+            if move_type == "psychict":
+                move_type = "psychic"
 
             html = response.text
             tables = pd.read_html(io=StringIO(html), match="Effect")
@@ -95,39 +115,31 @@ class Command(BaseCommand):
                 "contest_type",
             ]
 
-            moves = move_table.apply(
-                lambda r: Move(
-                    name=r["name"],
-                    effect=r["effect"],
-                    move_type=move_type.upper(),
-                    power=(int(r["power"]) if r["power"] != "--" else 0),
-                    accuracy=(int(r["accuracy"]) if r["accuracy"] != "--" else 0),
-                    pp=r["PP"],
-                ),
-                axis=1,
-            )
+            def build_move(row, pairs):
+                key = row['name'].lower().replace(' ', '')
+                id = None
+                for pair in pairs:
+                    if fuzz.ratio(pair[0], key) > 90:
+                        id = pair[1]
+                        break
 
+                return Move(
+                    id=id,
+                    name=row["name"],
+                    effect=row["effect"],
+                    move_type=move_type.upper(),
+                    power=(int(row["power"]) if row["power"] != "--" else 0),
+                    accuracy=(int(row["accuracy"]) if row["accuracy"] != "--" else 0),
+                    pp=row["PP"],
+                ),
+
+            moves = move_table.apply(lambda r: build_move(r, move_pairings), axis=1)
             new_moves += list(moves)
 
-        bulbapedia_url = 'https://bulbapedia.bulbagarden.net/wiki/List_of_moves'
-
-        response = httpx.get(bulbapedia_url)
-        html = response.text
-
-        # grab move table
-        move_table = pd.read_html(io=StringIO(html), match="Pound")[0].drop([0, 1], axis=1)
-        move_table.columns = [
-            'id', 'name', 'move_type', 'category', 'pp', 'power', 'accuracy', 'generation'
-        ]
-        move_table = move_table[
-            move_table["generation"].str.contains("^I$|^II$|^III$", regex=True)
-        ]
-        print(move_table)
+        print(new_moves)
 
         # insert them into the database
         # Move.objects.bulk_create(new_moves)
-
-
 
     def handle(self, *args, **options):
         # self._load_items()
--- core/models.py
+++ core/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Move(models.Model):
+    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=12)
     effect = models.CharField(max_length=511)
     pp = models.IntegerField()
@@ -10,7 +11,7 @@ class Move(models.Model):
     move_type = models.CharField(max_length=127)
 
     def __str__(self):
-        return f'{{ "name": "{self.name}", "effect": "{self.effect}", "pp": {self.pp}, "power": "{self.power}", "accuracy": "{self.accuracy}", "move_type": "{self.move_type}" }}'
+        return f'{{ "id": "{self.id}", "name": "{self.name}", "effect": "{self.effect}", "pp": {self.pp}, "power": "{self.power}", "accuracy": "{self.accuracy}", "move_type": "{self.move_type}" }}'
 
 
 # TODO: add abilities...
