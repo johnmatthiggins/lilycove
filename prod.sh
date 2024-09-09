cd837ba8b2d83c116d8894ccc5e256b6bd27d230
John M. Higgins <johnmatthiggins@gmail.com>
2024-09-09 06:01:28 +0000

    fixed prod script

--- prod.sh
+++ prod.sh
@@ -3,5 +3,12 @@
 # Script that runs production Django server on port specified by ENV
 HOST=127.0.0.1
 PORT=8000
+cd "$(dirname $0)";
 
-poetry run python -m gunicorn lilycove.asgi:application -w 8 -k uvicorn_worker.UvicornWorker
+test -f /root/.local/bin/poetry && export PATH="$PATH:/root/.local/bin/";
+
+if which poetry; then
+	poetry run python -m gunicorn lilycove.asgi:application -w 8 -k uvicorn_worker.UvicornWorker
+else
+	/root/.local/bin/poetry run python -m gunicorn lilycove.asgi:application -w 8 -k uvicorn_worker.UvicornWorker
+fi
