d42b94d8d33f35d3dcc32631ee7943ab2a443dc6
John Higgins <johnmatthiggins@gmail.com>
2024-09-09 12:38:35 -0700

    wrote deployment script

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/sh
+systemctl stop lilycove.xyz
+sh ./build.sh
+poetry run python manage.py collectstatic
+rm -rf /var/www/static/lilycove.xyz
+cp -r static/* /var/www/static/lilycove.xyz
+systemctl start lilycove.xyz
