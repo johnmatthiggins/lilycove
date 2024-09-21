4b6f11505a30e78e21279180a48aa0e45550474d
John Higgins <johnmatthiggins@gmail.com>
2024-09-20 23:46:07 -0700

    replaced spans with buttons

--- ui/src/PPCounter.jsx
+++ ui/src/PPCounter.jsx
@@ -8,17 +8,19 @@ function PPCounter({
   return (
     <div class="font-mono flex justify-between pl-2 select-none gap-2">
       <Show when={ppUpCount() > 0} fallback={
-        <span
+        <button
+          tabindex={0}
           class="rounded-md w-8 text-center hover:cursor-not-allowed shadow-sm border border-solid border-gray-400 bg-gray-200"
           onClick={decrement}>
           {'-'}
-        </span>
+        </button>
       }>
-        <span
+        <button
+          tabindex={0}
           class="bg-white rounded-md w-8 text-center shadow-sm border border-solid border-gray-400 hover:cursor-pointer"
           onClick={decrement}>
           {'-'}
-        </span>
+        </button>
       </Show>
       <span>{ppUpCount()}</span>
       <span class="h-1.5 rounded-md">
@@ -27,13 +29,15 @@ function PPCounter({
         </Show>
       </span>
       <Show when={ppUpCount() < 3} fallback={
-        <span
+        <button
+          tabindex={0}
           class="bg-gray-200 rounded-md w-8 text-center hover:cursor-not-allowed shadow-sm border border-solid border-gray-400"
           onClick={increment}>
           {'+'}
-        </span>
+        </button>
       }>
         <span
+          tabindex={0}
           class="bg-white rounded-md w-8 text-center shadow-sm border border-solid border-gray-400 hover:cursor-pointer"
           onClick={increment}>
           {'+'}
