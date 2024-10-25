81e265673a99df563539f631f147a8e91fee2926
John Higgins <johnmatthiggins@gmail.com>
2024-10-24 20:03:41 -0700

    added models to admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+class MoveAdmin(admin.ModelAdmin):
+    pass
+
+class SpeciesAdmin(admin.ModelAdmin):
+    pass
+
+class AbilityAdmin(admin.ModelAdmin):
+    pass
+
+class ItemAdmin(admin.ModelAdmin):
+    pass
+
+admin.register(models.Move, MoveAdmin)
+admin.register(models.Species, SpeciesAdmin)
+admin.register(models.Ability, AbilityAdmin)
+admin.register(models.Item, ItemAdmin)
